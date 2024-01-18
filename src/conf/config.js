f2eb51d2b5a494dd5ebfa11a9e1e6e7a900da8d5
yashasvisxena <yashasvi1802@gmail.com>
2024-01-19 03:33:11 +0530

    created config file

---
+++ src/conf/config.js
@@ -0,0 +1,9 @@
+const conf ={
+    appwriteUrl:String(import.meta.env.VITE_APPWRITE_URL),
+    projectId:String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    databaseId:String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    teacherId:String(import.meta.env.VITE_APPWRITE_TEACHER_ID),
+    studentId:String(import.meta.env.VITE_APPWRITE_STUDENT_ID),
+}
+
+export default conf ;
