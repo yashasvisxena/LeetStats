e6c7c5270c31faa0c508985fcf4348a10ba3af23
yashasvisxena <yashasvi1802@gmail.com>
2024-05-19 21:09:40 +0530

    docker initiated

--- src/main.jsx
+++ src/main.jsx
@@ -3,47 +3,73 @@ import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./Store/Store.js";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Home from "./components/Home/Home.jsx"
-import Login from "./components/Header/Login.jsx"
-import Dashboard from "./components/Dashboard/Dashboard.jsx"
-import Signup from "./components/Header/Signup.jsx"
-import AuthLayout from "./components/Header/AuthLayout.jsx"
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+import Home from "./components/Home/Home.jsx";
+import Login from "./components/Header/Login.jsx";
+import Dashboard from "./components/Dashboard/Dashboard.jsx";
+import Signup from "./components/Header/Signup.jsx";
+import AuthLayout from "./components/Header/AuthLayout.jsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      { path: "", element: <Home /> },
-      {
-        path: "dashboard",
-        element: (
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App />,
+//     children: [
+//       { path: "", element: <Home /> },
+//       {
+//         path: "dashboard",
+//         element: (
+//           <AuthLayout authentication>
+//             <Dashboard />
+//           </AuthLayout>
+//         ),
+//       },
+//       {
+//         path: "login",
+//         element: (
+//           <AuthLayout authentication={false}>
+//             <Login />
+//           </AuthLayout>
+//         ),
+//       },
+//       {
+//         path: "signup",
+//         element: (
+//           <AuthLayout authentication={false}>
+//             <Signup />
+//           </AuthLayout>
+//         ),
+//       },
+//     ],
+//   },
+// ]);
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="" element={<Home />} />
+      <Route
+        path="dashboard"
+        element={
           <AuthLayout authentication>
             <Dashboard />
           </AuthLayout>
-        ),
-      },
-      {
-        path: "login",
-        element: (
-          <AuthLayout authentication={false}>
+        }
+      />
+      <Route path="login" element={<AuthLayout authentication={false}>
             <Login />
-          </AuthLayout>
-        ),
-      },
-      {
-        path: "signup",
-        element: (
-          <AuthLayout authentication={false}>
+          </AuthLayout>} />
+      <Route path="signup" element={<AuthLayout authentication={false}>
             <Signup />
-          </AuthLayout>
-        ),
-      },
-    ],
-  },
-]);
-
+          </AuthLayout>} />
+    </Route>
+  )
+);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <RouterProvider router={router} />
