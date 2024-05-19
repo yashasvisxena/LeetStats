a41b35c122974aa414b0f6aab6e0aca13597e9a8
yashasvisxena <yashasvi1802@gmail.com>
2024-05-19 15:01:12 +0530

    path updated

--- src/main.jsx
+++ src/main.jsx
@@ -15,7 +15,7 @@ const router = createBrowserRouter([
     path: "/",
     element: <App />,
     children: [
-      { path: "", element: <Home /> },
+      { path: "/", element: <Home /> },
       {
         path: "/dashboard",
         element: (
