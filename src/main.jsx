a0b175ec758518c68af7632144cc0dc5209e620b
yashasvisxena <yashasvi1802@gmail.com>
2024-05-19 14:46:34 +0530

    main updated

--- src/main.jsx
+++ src/main.jsx
@@ -46,8 +46,6 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <RouterProvider router={router}>
-      <App />
-    </RouterProvider>
+    <RouterProvider router={router} />
   </Provider>
 );
