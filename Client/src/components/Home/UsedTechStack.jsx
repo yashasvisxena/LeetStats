3ae442a6e25228b69e51055851f2ddeb75e47775
nikhilpat123 <nik111pathak@gmail.com>
2024-07-25 16:06:23 +0530

    home page done

--- Client/src/components/Home/UsedTechStack.jsx
+++ Client/src/components/Home/UsedTechStack.jsx
@@ -2,22 +2,22 @@
 import React from 'react';
 
 const techStack = [
-  { name: 'React', logo: '/images/react-logo.png' },
-  { name: 'Redux', logo: '/images/redux-logo.png' },
-  { name: 'Appwrite', logo: '/images/appwrite-logo.png' },
-  { name: 'GraphQL', logo: '/images/graphql-logo.png' },
-  { name: 'Apollo', logo: '/images/apollo-logo.png' },
-  { name: 'Express', logo: '/images/express-logo.png' },
-  { name: 'Tailwind CSS', logo: '/images/tailwind-logo.png' },
-  { name: 'Shadcn', logo: '/images/shadcn-logo.png' },
+  { name: 'React', logo: 'src/components/Home/reactd.png' },
+  { name: 'Redux', logo: 'src/components/Home/redux.png' },
+  { name: 'Appwrite', logo: 'src/components/Home/appwrite.png' },
+  { name: 'GraphQL', logo: 'src/components/Home/graphQl.jpg' },
+  { name: 'Apollo', logo: 'src/components/Home/apollo.png' },
+  { name: 'Express', logo: 'src/components/Home/expressjs.png' },
+  { name: 'Tailwind CSS', logo: 'src/components/Home/tailwind.png' },
+  { name: 'Shadcn', logo: 'src/components/Home/139895814.png' },
 ];
 
 const UsedTechStack = () => {
   return (
-    <div className="flex flex-wrap justify-center p-6 bg-white rounded-lg shadow-lg max-w-4xl mx-auto my-8">
-      <h2 className="w-full text-3xl font-bold mb-4 text-center">Used Tech Stack</h2>
+    <div className=" bg-background flex flex-wrap justify-center p-6 rounded-lg shadow-lg max-w-4xl mx-auto my-8">
+      <h2 className=" text-ground w-full text-3xl font-bold mb-4 text-center">Used Tech Stack</h2>
       {techStack.map((tech, index) => (
-        <div key={index} className="flex flex-col items-center m-4 transform transition-transform duration-300 hover:scale-110">
+        <div key={index} className=" flex flex-col items-center m-4 transform transition-transform duration-300 hover:scale-110">
           <img src={tech.logo} alt={tech.name} className="h-16 w-16 mb-2" />
           <span className="text-lg font-medium">{tech.name}</span>
         </div>
