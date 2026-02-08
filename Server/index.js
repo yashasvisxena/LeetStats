6e88e8791af01104ebca01620633e8ff23c4912f
yashasvisxena <yashasvi1802@gmail.com>
2026-02-08 19:40:45 +0530

    cors fix

--- Server/index.js
+++ Server/index.js
@@ -1,18 +1,18 @@
-import express from 'express';
-import { ApolloServer } from '@apollo/server';
-import { expressMiddleware } from '@apollo/server/express4';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import axios from 'axios';
-import crypto from 'crypto';
+import express from "express";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import cors from "cors";
+import bodyParser from "body-parser";
+import axios from "axios";
+import crypto from "crypto";
 
 // Define the HMAC generation function
 function hmac_rawurlsafe_base64_string(distinct_id, secret) {
   return crypto
-    .createHmac('sha256', secret)
+    .createHmac("sha256", secret)
     .update(distinct_id)
-    .digest('base64url')
-    .replace(/=+$/, '');
+    .digest("base64url")
+    .replace(/=+$/, "");
 }
 
 const INBOX_SECRET = "W_F0I04NaLzhJEaEhuzRO3y7YCwJaLSbCXi973DHHe0";
@@ -65,32 +65,34 @@ async function startServer() {
               headers: {
                 "Content-Type": "application/json",
               },
-            }
-          )
+            },
+          ),
         );
 
         const responses = await Promise.all(requests);
-        return responses.map((response) => {
-          const userData = response.data.data.matchedUser;
-          if (!userData) {
-            return null; // or handle the error as appropriate
-          }
-          const submissionData = userData.submitStats.acSubmissionNum;
+        return responses
+          .map((response) => {
+            const userData = response.data.data.matchedUser;
+            if (!userData) {
+              return null; // or handle the error as appropriate
+            }
+            const submissionData = userData.submitStats.acSubmissionNum;
 
-          const solvedCounts = submissionData.reduce(
-            (obj, { difficulty, count }) => {
-              obj[difficulty.toLowerCase()] = count;
-              return obj;
-            },
-            { all: 0, easy: 0, medium: 0, hard: 0 }
-          );
+            const solvedCounts = submissionData.reduce(
+              (obj, { difficulty, count }) => {
+                obj[difficulty.toLowerCase()] = count;
+                return obj;
+              },
+              { all: 0, easy: 0, medium: 0, hard: 0 },
+            );
 
-          return {
-            studentName: userData.profile.realName,
-            studentUsername: userData.username,
-            ...solvedCounts,
-          };
-        }).filter(student => student !== null); // filter out null responses
+            return {
+              studentName: userData.profile.realName,
+              studentUsername: userData.username,
+              ...solvedCounts,
+            };
+          })
+          .filter((student) => student !== null); // filter out null responses
       },
     },
   };
@@ -98,20 +100,27 @@ async function startServer() {
   const server = new ApolloServer({ typeDefs, resolvers });
 
   app.use(bodyParser.json());
-  app.use(cors());
+  app.use(
+    cors({
+      origin: "https://leetstats.yashasvisaxena.in", // Adjust this to your frontend URL
+    }),
+  );
 
   await server.start();
 
-  app.use('/graphql', expressMiddleware(server));
+  app.use("/graphql", expressMiddleware(server));
 
-  app.get('/generate-subscriber-id', (req, res) => {
+  app.get("/generate-subscriber-id", (req, res) => {
     const distinct_id = req.query.distinct_id;
 
     if (!distinct_id) {
-      return res.status(400).send('distinct_id is required');
+      return res.status(400).send("distinct_id is required");
     }
 
-    const subscriber_id = hmac_rawurlsafe_base64_string(distinct_id, INBOX_SECRET);
+    const subscriber_id = hmac_rawurlsafe_base64_string(
+      distinct_id,
+      INBOX_SECRET,
+    );
     res.json({ subscriber_id });
   });
 
